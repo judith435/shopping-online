c077c94e7d89abadf26d8881f4768adbbbaa4a0d
יהודית אילסון <JudithI@kkl.org.il>
2018-02-06 13:22:21 +0200

    savush

--- client/products/products.controller.js
+++ client/products/products.controller.js
@@ -1,4 +1,11 @@
 shoppingApp.controller('productController', function handleProducts($scope)
 {
+    $scope.productName = 'click +  to add product';
+    $scope.showProductUpdate = false;
+
+    $scope.addProduct = function()  {
+        $scope.showProductUpdate = true;
+        $scope.productName_errorMessage = 'product name required';
+    }  
 
 });
