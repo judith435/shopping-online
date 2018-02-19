a219a200ef247b5421699f48310356d240f4dd40
judith435 <judithguttmann@gmail.com>
2018-02-19 19:04:09 +0200

    save...

--- server/bl/loginBL.js
+++ server/bl/loginBL.js
@@ -11,12 +11,11 @@ function login(loginDetails, callback) {
     dal.executeQuery('shopping', 'get_customer_info', spParms, function(err, rows) {
         if (err) {
             callback('called by loginBL.login => ' + err);
+            return;
         }
-        else {
-            //const customerInfo = {}; //rows[0][0].street
-            const customerInfo = new model.Customer(rows[0][0]);
-            callback(null, customerInfo);
-        }
+
+        const customerInfo = rows[0][0] ? new model.Customer(rows[0][0]) : 'no user found with these login details';
+        callback(null, customerInfo);
     });
 }
 
