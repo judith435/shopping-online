2311589acdcd534150a9aed3fbe3bc9d85f7e042
judith435 <judithguttmann@gmail.com>
2018-02-21 23:11:45 +0200

    save...

--- client/shopping-cart/ctrlCartUpdate.js
+++ client/shopping-cart/ctrlCartUpdate.js
@@ -1,16 +1,36 @@
 shoppingApp.controller('ctrlCartUpdate', function updateProducts($scope,
     // $rootScope, 
+                                                                $modal,
                                                                 // productService, 
                                                                 imageService, 
                                                                 configSettings,
                                                                 $filter)
 {
 
- $scope.cartOwner = 'My Cart: ' + $scope.customer.firstName + ' ' + $scope.customer.lastName
+    $scope.cartOwner = 'My Cart: ' + $scope.customer.firstName + ' ' + $scope.customer.lastName
 
-$scope.$on('product-selected', function(event, product) {
-    alert ('in ctrlCartUpdate => product: ' + JSON.stringify(product));
-});
+    $scope.$on('product-selected', function(event, product) {
+        alert ('in ctrlCartUpdate => product: ' + JSON.stringify(product));
+        var productDialog = $modal.open({
+            templateUrl: 'popup.html',
+            controller: 'productDialogCtrl',
+            size: 'lg',
+            resolve: {
+              selectedUsr: function () {
+                return $scope.usr;
+              }
+            }
+          });
+
+          productDialog.result.then(function (newusr) {
+            $scope.usrs.push(newusr);
+            $scope.usr = {name: '', job: '', age: '', sal: '', addr:''};
+        });
+
+
+
+
+    });
 
 
 
