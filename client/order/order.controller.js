ef51d8c5e7a5d2fc13702ea8e63af26dbc0b544b
judith435 <judithguttmann@gmail.com>
2018-03-08 00:09:07 +0200

    codacy 20

--- client/order/order.controller.js
+++ client/order/order.controller.js
@@ -65,9 +65,9 @@ shoppingApp.controller("ctrlOrder", function signUp($scope,
             var creditCard = $scope.order.creditCard.replace(/\s/g, "");
             var ccRegex = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/;
             var ccValid = ccRegex.test(creditCard);
-            if (ccValid) {
+            // if (ccValid) {
                 ccValid =  validateCC(creditCard);
-            }
+            // }
             $scope.creditCardErrorMessage = !ccValid  ? "Invalid Credit Card" : "";
             $scope.errorsFound = !ccValid || $scope.errorsFound;
             return;
