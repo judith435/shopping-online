706964ee1692b104a3c1ee4feea9b20f437054d2
judith435 <judithguttmann@gmail.com>
2018-03-17 22:05:10 +0200

    codacy 36

--- client/products/directives/fileModelDirective.js
+++ client/products/directives/fileModelDirective.js
@@ -15,17 +15,3 @@ shoppingApp.directive("fileModel", ["$parse", function ($parse) {
        }
     };
  }]);
- 
-
-//  link: function(scope, element, attrs) {
-//     var model = $parse(attrs.fileModel);
-//     var modelSetter = model.assign;
-    
-//     element.bind("change", function(){
-//        scope.$apply(function(){
-//           var fileUploaded = element[0].files[0];
-//           modelSetter(scope, fileUploaded);
-//           scope.imageUploaded();
-//        });
-//     });
-//  }
